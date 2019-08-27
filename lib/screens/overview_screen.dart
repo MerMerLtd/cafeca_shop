63fd04dc9e67f6516ee4aec752627a0b0e33cc68
EmilyLiang <hazelliang@Emily-Liang-de-MacBook-Pro.local>
2019-08-27 15:20:11 +0800

    customed modal

--- lib/screens/overview_screen.dart
+++ lib/screens/overview_screen.dart
@@ -3,75 +3,107 @@ import 'package:intl/intl.dart';
 
 import '../screens/daily_revenues_screen.dart';
 import '../screens/monthly_revenues_screen.dart';
+import '../screens/redeem_item_screen.dart';
 import '../widgets/shop_tile.dart';
 import '../widgets/border_all_text.dart';
 import '../widgets/bubble_tarbar.dart';
 
-class OverviewScreen extends StatelessWidget {
+class OverviewScreen extends StatefulWidget {
+  @override
+  _OverviewScreenState createState() => _OverviewScreenState();
+}
+
+class _OverviewScreenState extends State<OverviewScreen> {
   final String shopName = 'Cafeca Coffee Shop';
+
   final String imageUrl =
       'https://images.unsplash.com/photo-1546541865-1cb39bcd0c83?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80';
 
-  // const OverviewScreen({this.shopName, this.imageUrl});
+  bool isModalOpen = false;
+
+  void toggleModal(bool state) {
+    setState(() {
+      isModalOpen = state;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // final _deviceData = MediaQuery.of(context).size;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Overview'),
-      ),
-      body: DefaultTabController(
-        length: 3,
-        child: Container(
-          margin: EdgeInsets.symmetric(
-              horizontal: 60, vertical: 10), // whole page margin
-          child: Column(
-            children: <Widget>[
-              //--- Shop image && Shop name
-              ShopTile(shopName: shopName, imageUrl: imageUrl),
-              //--- Divider
-              Divider(),
-              SizedBox(height: 20),
-              //--- tab Bar
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Stack(
+      children: <Widget>[
+        Scaffold(
+          appBar: AppBar(
+            title: Text('Overview'),
+          ),
+          body: DefaultTabController(
+            length: 3,
+            child: Container(
+              margin: EdgeInsets.symmetric(
+                  horizontal: 60, vertical: 10), // whole page margin
+              child: Column(
                 children: <Widget>[
-                  Flexible(
-                    flex: 1,
-                    child: BubbleTabBar(
-                      tabs: [
-                        Tab(child: BorderAllText(text: '本日營收')),
-                        Tab(child: BorderAllText(text: '月結資訊')),
-                        Tab(child: BorderAllText(text: '成長狀況')),
-                      ],
-                    ),
-                  ),
-                  Flexible(
-                    flex: 1,
-                    child: Text(
-                      DateFormat('yyyy-MM-dd').format(
-                        DateTime.now(),
+                  //--- Shop image && Shop name
+                  ShopTile(shopName: shopName, imageUrl: imageUrl),
+                  //--- Divider
+                  Divider(),
+                  SizedBox(height: 20),
+                  //--- tab Bar
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Flexible(
+                        flex: 1,
+                        child: BubbleTabBar(
+                          tabs: [
+                            Tab(child: BorderAllText(text: '本日營收')),
+                            Tab(child: BorderAllText(text: '月結資訊')),
+                            Tab(child: BorderAllText(text: '成長狀況')),
+                          ],
+                        ),
                       ),
-                      style: TextStyle(fontSize: 20.0),
+                      Flexible(
+                        flex: 1,
+                        child: Text(
+                          DateFormat('yyyy-MM-dd').format(
+                            DateTime.now(),
+                          ),
+                          style: TextStyle(fontSize: 20.0),
+                        ),
+                      ),
+                    ],
+                  ),
+                  // ---TabBarView
+                  SizedBox(height: 20),
+                  Expanded(
+                    child: TabBarView(
+                      children: [
+                        DailyRevenuesScreen(),
+                        MonthlyRevenuesScreen(),
+                        Icon(Icons.directions_bike),
+                      ],
                     ),
                   ),
                 ],
               ),
-              // ---TabBarView
-              SizedBox(height: 20),
-              Expanded(
-                child: TabBarView(
-                  children: [
-                    DailyRevenuesScreen(),
-                    MonthlyRevenuesScreen(),
-                    Icon(Icons.directions_bike),
-                  ],
-                ),
-              ),
-            ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              toggleModal(true);
+            },
           ),
         ),
-      ),
+        if (isModalOpen)
+          RedeemItemScreen(
+            onCancelBtnPressed: () {
+              toggleModal(false);
+            },
+            onCheckBtnPressed: () {
+              toggleModal(false);
+            },
+          ),
+      ],
     );
   }
 }
---
+++ lib/screens/redeem_item_screen.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+class RedeemItemScreen extends StatelessWidget {
+  final Function onCheckBtnPressed;
+  final Function onCancelBtnPressed;
+
+  const RedeemItemScreen({
+    Key key,
+    @required this.onCancelBtnPressed,
+    @required this.onCheckBtnPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black38,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 100, horizontal: 60),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Flex(
+              direction: Axis.horizontal,
+              children: <Widget>[
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    decoration: BoxDecoration(color: Colors.black12),
+                    child: Column(
+                      children: <Widget>[],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 5,
+                  child: Stack(
+                    children: <Widget>[
+                      Flex(
+                        direction: Axis.vertical,
+                        children: <Widget>[
+                          Expanded(
+                            flex: 4,
+                            child: Container(
+                              alignment: Alignment.center,
+                              margin: EdgeInsets.only(top:60,bottom: 30),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(5),
+                                child: Image.network(
+                                    'https://images.unsplash.com/photo-1493807879848-13f1500cee7f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80'),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 3,
+                            child: Container(
+                              padding: EdgeInsets.symmetric(horizontal: 60),
+                              child: GridView.builder(
+                                padding: const EdgeInsets.all(10.0),
+                                itemCount: 10,
+                                itemBuilder: (ctx, i) => ClipRRect(
+                                  borderRadius: BorderRadius.circular(100),
+                                  child: RaisedButton(
+                                    onPressed: () {},
+                                    child: Text(i.toString()),
+                                  ),
+                                ),
+                                gridDelegate:
+                                    SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 3,
+                                  childAspectRatio: 8 / 2,
+                                  crossAxisSpacing: 20,
+                                  mainAxisSpacing: 20,
+                                ),
+                                // scrollDirection: Axis.horizontal,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        // color: Colors.pink,
+                        width: double.infinity,
+                        child: Align(
+                          alignment: Alignment.bottomRight,
+                          child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Row(
+                              children: <Widget>[
+                                Spacer(),
+                                FlatButton(
+                                  shape: CircleBorder(),
+                                  color: Theme.of(context).primaryColor,
+                                  child: Icon(
+                                    Icons.close,
+                                    size: 60,
+                                    color: Colors.white,
+                                  ),
+                                  onPressed: onCancelBtnPressed,
+                                ),
+                                FlatButton(
+                                  shape: CircleBorder(),
+                                  color: Theme.of(context).primaryColor,
+                                  child: Icon(
+                                    Icons.check,
+                                    size: 60,
+                                    color: Colors.white,
+                                  ),
+                                  onPressed: onCheckBtnPressed,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
